b821c6e9cf77d88d89ed1d5a36383e1b22546f12
Kartik Banik <kartikbanikshishir@gmail.com>
2023-08-11 01:19:23 +0600

    field_c

--- u_project/lib/app/modules/service_ui/views/receive_tab_bar_view.dart
+++ u_project/lib/app/modules/service_ui/views/receive_tab_bar_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:u_project/widgets/custom_text.dart';
 
 class ReceiveView extends StatefulWidget {
   @override
@@ -7,7 +8,7 @@ class ReceiveView extends StatefulWidget {
 }
 
 class _ReceiveViewState extends State<ReceiveView> {
-  final _formKey = GlobalKey<FormState>();
+  // final _formKey = GlobalKey<FormState>();
 
   TextEditingController nameController = TextEditingController();
   TextEditingController phoneController = TextEditingController();
@@ -43,17 +44,19 @@ class _ReceiveViewState extends State<ReceiveView> {
             child: ListView(
               children: <Widget>[
                 TextFormField(
+                  keyboardType: TextInputType.name,
                   controller: nameController,
                   decoration: const InputDecoration(
-                    icon: const Icon(Icons.person),
+                    icon: Icon(Icons.person),
                     hintText: 'Enter your name',
                     labelText: 'Name',
                   ),
                 ),
                 TextFormField(
                   controller: phoneController,
+                  keyboardType: TextInputType.number,
                   decoration: const InputDecoration(
-                    icon: const Icon(Icons.phone),
+                    icon: Icon(Icons.phone),
                     hintText: 'Enter a phone number',
                     labelText: 'Phone',
                   ),
@@ -75,16 +78,19 @@ class _ReceiveViewState extends State<ReceiveView> {
                 ),
                 TextFormField(
                   controller: bloodGrpController,
+                  keyboardType: TextInputType.text,
                   decoration: const InputDecoration(
-                    icon: const Icon(Icons.bloodtype),
+                    icon: Icon(Icons.bloodtype),
                     hintText: 'Select your blood group',
                     labelText: 'Blood Group',
                   ),
                 ),
                 TextFormField(
                   controller: addressController,
+                  keyboardType: TextInputType.text,
                   decoration: const InputDecoration(
-                    icon: const Icon(Icons.location_city),
+                    suffixIcon: Icon(Icons.pin_drop),
+                    icon: Icon(Icons.location_city),
                     hintText: 'Your Address',
                     labelText: 'Location',
                   ),
@@ -108,9 +114,89 @@ class _ReceiveViewState extends State<ReceiveView> {
     return Scaffold(
       floatingActionButton: FloatingActionButton.extended(
         onPressed: _showBottomSheet,
-        label: const Text("POST"),
+        backgroundColor: Colors.redAccent,
+        label: Custom_TExt(
+          txt: "POST",
+          fs: 16,
+          fw: FontWeight.bold,
+        ),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Custom_TExt(
+                txt: "Request For Blood",
+                fs: 23,
+                fw: FontWeight.bold,
+              ),
+            ),
+            Expanded(
+              flex: 10,
+              child: Container(
+                child: ListView(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height / 5,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      child: Column(children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            radius: 26,
+                            backgroundColor: Colors.black,
+                            child: CircleAvatar(
+                              radius: 25,
+                              backgroundColor: Colors.white,
+                              child: Custom_TExt(
+                                txt: "A+",
+                                textColor: Colors.red,
+                                fs: 18,
+                                fw: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          title: Text('Card Title'),
+                          subtitle: Text('Card Subtitle'),
+                        ),
+                        ButtonBar(
+                          children: <Widget>[
+                            TextButton(
+                              child: Text('ACTION 1'),
+                              onPressed: () {
+                                // Perform action 1
+                              },
+                            ),
+                            TextButton(
+                              child: Text('ACTION 2'),
+                              onPressed: () {
+                                // Perform action 2
+                              },
+                            ),
+                          ],
+                        ),
+                      ]),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
