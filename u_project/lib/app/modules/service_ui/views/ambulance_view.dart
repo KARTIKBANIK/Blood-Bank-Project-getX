f37f99f87f708c58b85e6475265ad251874e844e
Kartik Banik <kartikbanikshishir@gmail.com>
2023-08-13 17:14:06 +0600

    list_tile

--- u_project/lib/app/modules/service_ui/views/ambulance_view.dart
+++ u_project/lib/app/modules/service_ui/views/ambulance_view.dart
@@ -1,20 +1,100 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:u_project/widgets/custom_text.dart';
 
 class AmbulanceView extends GetView {
   const AmbulanceView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    var name = [
+      "Rahim Halque",
+      "Tarikul Islam",
+      "Jahid Hasan",
+      "Monir Hossain",
+      "Mamun Islam",
+      "Rafique Sheikh",
+      "Jillur Rahman",
+      "Sahbuddin Islam",
+    ];
+    var phone = [
+      "+88 01862131292",
+      "+88 01761254478",
+      "+88 01811479654",
+      "+88 01362887924",
+      "+88 01925577789",
+      "+88 01854778952",
+      "+88 01745887933",
+      "+88 01862131295",
+    ];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('AmbulanceView'),
+        backgroundColor: Colors.redAccent,
+        title: const Text('Ambulance'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'AmbulanceView is working',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Lottie.asset("assets/animations/ambulance.json"),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 2,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    width: .7,
+                    color: Colors.black,
+                  ),
+                ),
+                child: ListView.builder(
+                  itemCount: name.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        elevation: 2,
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.blueGrey,
+                            child: Icon(
+                              Icons.person,
+                            ),
+                          ),
+                          title: Text(
+                            name[index],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(
+                            phone[index],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          trailing: IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.call,
+                                color: Colors.lightGreen,
+                              )),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- u_project/lib/app/modules/service_ui/views/doctor_view.dart
+++ u_project/lib/app/modules/service_ui/views/doctor_view.dart
@@ -1,20 +1,122 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 
 class DoctorView extends GetView {
   const DoctorView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    var name = [
+      "Dr. Partha Dev Roy",
+      "Dr. Pulak Nath Banik",
+      "Dr. Jahid Hasan",
+      "Dr. Musa Ibrahim",
+      "Dr. Anamul Hasib",
+      "Dr. Lucky Begum",
+      "Dr. Jillur Rahman",
+      "Dr. Sahbuddin Islam",
+    ];
+    var specialist = [
+      "Medichine Speacialist",
+      "Scin Speacialist",
+      "Gyney Speacialist",
+      "Kydney Speacialist",
+      "Heart Speacialist",
+      "Child Speacialist",
+      "Child Speacialist",
+      "Kydney Speacialist",
+      "Medichine Speacialist",
+    ];
+    var phone = [
+      "+88 01862131292",
+      "+88 01761254478",
+      "+88 01811479654",
+      "+88 01362887924",
+      "+88 01925577789",
+      "+88 01854778952",
+      "+88 01745887933",
+      "+88 01862131295",
+    ];
     return Scaffold(
       appBar: AppBar(
-        title: const Text('DoctorView'),
+        backgroundColor: Colors.redAccent,
+        title: const Text('Doctor'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'DoctorView is working',
-          style: TextStyle(fontSize: 20),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Lottie.asset("assets/animations/doctor.json"),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 2,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    width: .7,
+                    color: Colors.black,
+                  ),
+                ),
+                child: ListView.builder(
+                  itemCount: name.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        elevation: 2,
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.blueGrey,
+                            child: Icon(
+                              Icons.person,
+                            ),
+                          ),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                name[index],
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                specialist[index],
+                                style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                            ],
+                          ),
+                          subtitle: Text(
+                            "Phone: ${phone[index]}",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          trailing: IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.call,
+                                color: Colors.lightGreen,
+                              )),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
